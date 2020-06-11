3283c8d016f22abb151f4cc577360e228debae1f
aniketbhaisare26 <aniketbhaisare1998@gmail.com>
2020-06-11 00:21:30 -0700

    parking management system

---
+++ vpms/includes/footer.php
@@ -0,0 +1,12 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-dark">
+                <div class="row">
+                    <div class="col-sm-6">
+                        Developed By Amit patil,Nikhil Bagde,Pooja Prajapati
+                    </div>
+                    <div class="col-sm-6 text-right">
+                         Parking Management System
+                    </div>
+                </div>
+            </div>
+  </footer>
